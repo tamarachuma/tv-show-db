bd2ae124bbf50386119bba9df11d61e01d4b13ba
Tamara Chuma <55622178+tamarachuma@users.noreply.github.com>
2022-02-16 14:07:11 +0400

    final commit update

--- app/src/main/java/com/project/popularmovies/ui/MainActivity.kt
+++ app/src/main/java/com/project/popularmovies/ui/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.project.popularmovies.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.project.popularmovies.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
